95f6c67de7289d73258550d421ac69e3c59d3a49
madhusudanrathi99 <madhusudanrathi99@gmail.com>
2024-08-29 12:34:12 +0530

    Update fs.copy to fs.copyFile for better performance

--- feedback_app/server.js
+++ feedback_app/server.js
@@ -36,7 +36,7 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.copy(tempFilePath, finalFilePath);
+      await fs.copyFile(tempFilePath, finalFilePath);
       await fs.unlink(tempFilePath);
       res.redirect('/');
     }
